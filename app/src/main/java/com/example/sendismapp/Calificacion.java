4c632fb319cbd7affcb38555ed598b581feb6757
datovarh <datovarh@unal.edu.co>
2020-03-01 00:49:19 -0500

    vista calificacion

---
+++ app/src/main/java/com/example/sendismapp/Calificacion.java
@@ -0,0 +1,37 @@
+package com.example.sendismapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+
+public class Calificacion extends AppCompatActivity {
+
+    RatingBar califica;
+    Dialog ver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calificacion);
+    }
+
+    public void clickRuta(View v){
+        ver = new Dialog(this);
+        ver.setContentView(R.layout.ly_calificacion);
+        califica = ver.findViewById(R.id.estrellas);
+        califica.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            // Called when the user swipes the RatingBar
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                if(rating == 0){
+                    califica.setRating(1);
+                }
+
+            }
+        });
+        ver.show();
+    }
+}
