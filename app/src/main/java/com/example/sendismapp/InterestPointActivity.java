17eae7b5fc0af2101d9f0da9468c58bd2529fb48
gamendez98 <gamendez98@gmail.com>
2020-03-01 16:41:24 -0500

    vista sitio de interes creada

---
+++ app/src/main/java/com/example/sendismapp/InterestPointActivity.java
@@ -0,0 +1,14 @@
+package com.example.sendismapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InterestPointActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interest_point);
+    }
+}
