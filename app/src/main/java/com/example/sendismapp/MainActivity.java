72b9664b730797c232157e9646fce0978e4b2800
datovarh <datovarh@unal.edu.co>
2020-03-01 19:09:03 -0500

    vista de filtros

--- app/src/main/java/com/example/sendismapp/MainActivity.java
+++ app/src/main/java/com/example/sendismapp/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //startActivity(new Intent(this,Calificacion.class)); //vistaCalificacion
+        //startActivity(new Intent(this,FiltrosBusqueda.class));//vistaFiltros
 
     }
 }
