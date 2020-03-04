024ed8d21a8b954cba8f198fb3335885b598526a
stivgo <60205335+stivgo@users.noreply.github.com>
2020-03-03 23:31:02 -0500

    Pantallas Compartir Ruta y ver Rutas

--- app/src/main/java/com/example/sendismapp/MainMenuActivity.java
+++ app/src/main/java/com/example/sendismapp/MainMenuActivity.java
@@ -2,13 +2,38 @@ package com.example.sendismapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
 
+    Button botonCrear;
+    Button botonRecorrer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        botonCrear = findViewById(R.id.button_create_route);
+        botonRecorrer = findViewById(R.id.button_recorrer);
+
+        botonCrear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, CrearRuta.class);
+                startActivity(intent);
+            }
+        });
+
+        botonRecorrer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, EditarRuta.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
