b3dd1740258c63d35cc2625820b224269c3057b8
Johan-Ortegon-1 <johandanielortegonparra@gmail.com>
2020-03-17 01:24:22 -0500

    Continuación patalla registrarse

---
+++ app/src/main/java/com/example/sendismapp/Registrarse.java
@@ -0,0 +1,15 @@
+package com.example.sendismapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registrarse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrarse);
+    }
+}
