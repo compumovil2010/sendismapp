8501102673c1a32cc71c37a1fb60db71af907aa1
stivgo <60205335+stivgo@users.noreply.github.com>
2020-04-22 04:33:21 -0500

    Vistas Stiven

--- app/src/main/java/com/example/sendismapp/Calificacion.java
+++ app/src/main/java/com/example/sendismapp/Calificacion.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
@@ -12,12 +14,14 @@ public class Calificacion extends AppCompatActivity {
 
     RatingBar califica;
     Dialog ver;
+    private Button botonCalificacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         ver = new Dialog(this);
+        botonCalificacion = findViewById(R.id.btHacerCalificacion);
         ver.setContentView(R.layout.ly_calificacion);
         califica = ver.findViewById(R.id.estrellas);
         califica.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
@@ -32,6 +36,13 @@ public class Calificacion extends AppCompatActivity {
         });
         ver.show();
         //Toast.makeText(this,String.valueOf(califica.getRating()),Toast.LENGTH_SHORT).show();
+        botonCalificacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+            }
+        });
     }
 
 }
