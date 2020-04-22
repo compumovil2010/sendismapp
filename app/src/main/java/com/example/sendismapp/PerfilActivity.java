2b51dd0c9c6d8697cd4e0e371db787e02fe4bc5f
stivgo <60205335+stivgo@users.noreply.github.com>
2020-04-22 04:21:19 -0500

    Vistas Stiven

--- app/src/main/java/com/example/sendismapp/PerfilActivity.java
+++ app/src/main/java/com/example/sendismapp/PerfilActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
@@ -16,11 +17,18 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class
 PerfilActivity extends AppCompatActivity {
@@ -29,6 +37,8 @@ PerfilActivity extends AppCompatActivity {
     static final int IMAGEN = 1;
     StorageReference mStorage;
     private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
     public static final String PATH_USERS="users/";
     public static final String PATH_USERS_IMAGES="images/";
     private SeekBar seekBarPeso;
@@ -40,6 +50,13 @@ PerfilActivity extends AppCompatActivity {
     private EditText editTextNombreUsuario;
     private EditText editTextNombre;
     private EditText editTextEdad;
+    private int edad;
+    private int peso;
+    private int altura;
+    private Button botonAplicar;
+    private Button botonContraseña;
+    private FirebaseUser user;
+
 
 
     @Override
@@ -49,6 +66,9 @@ PerfilActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         mStorage = FirebaseStorage.getInstance().getReference(PATH_USERS_IMAGES);
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference(PATH_USERS);
+
         imagenPerfil = findViewById(R.id.imagenPerfil);
         seekBarAltura = findViewById(R.id.seek_bar_altura);
         seekBarExperiencia = findViewById(R.id.seek_bar_experiencia);
@@ -59,15 +79,46 @@ PerfilActivity extends AppCompatActivity {
         editTextEdad = findViewById(R.id.edit_text_age);
         editTextNombre = findViewById(R.id.edit_text_name_profile);
         editTextNombreUsuario = findViewById(R.id.edit_text_username_profile);
+        botonAplicar= findViewById(R.id.button_aplicar);
+        botonContraseña = findViewById(R.id.button_contrasena);
 
 
-        seekBarExperiencia.setProgress(Integer.parseInt(textViewExperiencia.getText().toString()));
-        FirebaseUser user = mAuth.getCurrentUser();
+
+        user = mAuth.getCurrentUser();
         try {
             cargarFoto(user.getUid());
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        myRef.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists())
+                {
+                    String nombre = dataSnapshot.child("nombre").getValue().toString();
+                    editTextNombre.setText(nombre);
+                    editTextNombreUsuario.setText(nombre+"423");
+                    edad = Integer.parseInt(dataSnapshot.child("edad").getValue().toString());
+                    peso = Integer.parseInt(dataSnapshot.child("peso").getValue().toString());
+                    altura = Integer.parseInt(dataSnapshot.child("altura").getValue().toString());
+                    editTextEdad.setText(Integer.toString(edad));
+                    textViewPeso.setText(Integer.toString(peso) );
+                    textViewAltura.setText(Integer.toString(altura));
+                    seekBarPeso.setProgress(100*peso/200);
+                    seekBarAltura.setProgress(100*altura/230);
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        seekBarExperiencia.setProgress(Integer.parseInt(textViewExperiencia.getText().toString()));
 
         imagenPerfil.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,7 +131,7 @@ PerfilActivity extends AppCompatActivity {
         seekBarPeso.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textViewPeso.setText(Integer.toString(progress*2)+" Kg");
+                textViewPeso.setText(Integer.toString(progress*2));
 
             }
 
@@ -117,7 +168,7 @@ PerfilActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 int altura = progress*230/100;
-                textViewAltura.setText(Integer.toString(altura)+" cm");
+                textViewAltura.setText(Integer.toString(altura));
 
             }
 
@@ -129,6 +180,27 @@ PerfilActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
+            }
+        });
+
+        botonAplicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String,Object>usuarioMap = new HashMap<>();
+                usuarioMap.put("nombre",editTextNombre.getText().toString());
+                usuarioMap.put("edad",editTextEdad.getText().toString());
+                usuarioMap.put("peso",textViewPeso.getText().toString());
+                usuarioMap.put("altura",textViewAltura.getText().toString());
+                myRef.child(user.getUid()).updateChildren(usuarioMap);
+
+                finish();
+            }
+        });
+        botonContraseña.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PerfilActivity.this,CambiarContrasena.class);
+                startActivity(intent);
             }
         });
 
@@ -146,15 +218,7 @@ PerfilActivity extends AppCompatActivity {
 
                 Picasso.get().load(uri)
                         .into(imagenPerfil);
-                /*InputStream imageStream = null;
-                try {
-                    imageStream = getContentResolver().openInputStream(uri);
 
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-                imagenPerfil.setImageBitmap(selectedImage);*/
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -165,6 +229,5 @@ PerfilActivity extends AppCompatActivity {
 
 
     }
-
 
 }
