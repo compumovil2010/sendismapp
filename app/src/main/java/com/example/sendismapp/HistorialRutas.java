a310b673d8fb845228395f730629bb9af14e9855
Johan-Ortegon-1 <johandanielortegonparra@gmail.com>
2020-05-25 00:42:21 -0500

    Historial de rutas escribe Firebase lectura

--- app/src/main/java/com/example/sendismapp/HistorialRutas.java
+++ app/src/main/java/com/example/sendismapp/HistorialRutas.java
@@ -8,6 +8,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,7 @@ import com.example.sendismapp.logic.HistorialRuta;
 import com.example.sendismapp.logic.MiRuta;
 import com.example.sendismapp.logic.Ruta;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,33 +25,52 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class HistorialRutas extends AppCompatActivity {
 
     private Button botonCalificar;
     private Button botonComentar;
     private HistorialRuta[] misRutas;
-    private FirebaseAuth mAuth;
-    private FirebaseDatabase database;
-    private DatabaseReference myRef;
     public static final String PATH_ROUTES="rutas/";
     private ArrayList<Ruta> rutas;
+
+    private final static String PATH_HISOTIAL = "historialRutas/";
+
+    //Feeding the listView
+    private List<String> llavesRutasRecorridas = new ArrayList<String>();
+    private String rutasRecorridas = "";
+
+    //Firebase things
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private FirebaseUser user;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historial_rutas);
+        //Firebase
+        database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
         rutas = new ArrayList<Ruta>();
         botonCalificar = findViewById(R.id.buttonCalificar);
         botonComentar = findViewById(R.id.buttonComentar);
-        leerFB();
+        leerHistorial();
         Log.e("OMG", "Resultado lectura: " + rutas.size());
+        if(rutas.size() == 0)
+        {
+            Toast.makeText(this,"Usted no ha recorrido ninguna ruta", Toast.LENGTH_LONG).show();
+        }
 
 
 
         //iniciarArreglo();
-
-
         botonCalificar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -103,10 +124,10 @@ public class HistorialRutas extends AppCompatActivity {
     }
     private void leerFB()
     {
-
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference(PATH_ROUTES);
-        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+        myRef.addListenerForSingleValueEvent(new ValueEventListener()
+        {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot single : dataSnapshot.getChildren())
@@ -120,7 +141,8 @@ public class HistorialRutas extends AppCompatActivity {
                         aux.setLlavePropietario(single2.child("llavePropietario").getValue().toString());
                         aux.setLlaveRutaActual(single2.child("llaveRutaActual").getValue().toString());
                         aux.setNombre(single2.child("nombre").getValue().toString());
-                        rutas.add(aux);
+                        if(buscarRuta(aux.getLlaveRutaActual()))
+                            rutas.add(aux);
 
                         single.getKey();
                         Log.e("OMG", "Resultado lectura: " + single2.child("nombre").getValue().toString());
@@ -135,7 +157,39 @@ public class HistorialRutas extends AppCompatActivity {
 
             }
         });
+    }
+    public void leerHistorial()
+    {
+        myRef = database.getReference(PATH_HISOTIAL);
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
 
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                Map<String, Object> mapaHistorial = (Map<String, Object>) dataSnapshot.getValue();
+                Log.e("ERR23: ", "Encontro un ID: " + mapaHistorial.get(user.getUid()));
+                rutasRecorridas = (String) mapaHistorial.get(user.getUid());
+                if(rutasRecorridas!=null)
+                {
+                    String[] splitOn = rutasRecorridas.split(",");
+                    llavesRutasRecorridas.addAll(Arrays.asList(splitOn));
+                }
+                leerFB();
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    public boolean buscarRuta(String llaveRuta)
+    {
+        for(String iter : llavesRutasRecorridas)
+        {
+            if(iter.equals(llaveRuta))//La ruta fue recorrida por el usuario.
+                return true;
+        }
+        return false;
     }
 }
